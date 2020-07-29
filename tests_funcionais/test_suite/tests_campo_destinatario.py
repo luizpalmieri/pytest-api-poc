836499a0caa639d04985585fa18c4e5ac7d8d3f9
palmieri <luizgfpalmieri@gmail.com>
2020-07-28 23:24:21 -0300

    minor enhancements

--- tests_funcionais/test_suite/tests_campo_destinatario.py
+++ tests_funcionais/test_suite/tests_campo_destinatario.py
@@ -1,7 +1,5 @@
 import requests
-import json
-from test_utils import LambdaPayload
-from test_utils import LAMBDA_URL
+from test_utils import *
 
 
 class TestsPositivos:
@@ -10,8 +8,13 @@ class TestsPositivos:
         payload = LambdaPayload("olar", "41984266848")
         response = requests.request("POST", LAMBDA_URL, headers=payload.header, json=payload.body)
 
-        response_data = json.loads(response.content)
+        response_data = response.json()
 
         assert(response.status_code == 200)
         assert(response_data["statusCode"] == 200)
         assert(response_data["body"] == '"Mensagem enviada com sucesso"')
+
+        pretty_print_request(response.request)
+        pretty_print_response(response)
+
+
--- tests_funcionais/utils/test_utils.py
+++ tests_funcionais/utils/test_utils.py
@@ -2,6 +2,7 @@ import json
 
 LAMBDA_URL = "TBD"
 
+
 class LambdaPayload:
     def __init__(self, mensagem, num_destinatario):
         self.mensagem = mensagem
@@ -12,6 +13,23 @@ class LambdaPayload:
     def construir_body(self):
         return {"mensagem": self.mensagem, "num_destinatario": self.num_destinario}
 
-
     def construir_header(self):
         return {'Content-Type': 'application/json'}
+
+
+def pretty_print_request(request):
+    print('\n{}\n{}\n\n{}\n\n{}\n'.format(
+        '-----------Request----------->',
+        request.method + ' ' + request.url,
+        '\n'.join('{}: {}'.format(k, v) for k, v in request.headers.items()),
+        request.body)
+    )
+
+
+def pretty_print_response(response):
+    print('\n{}\n{}\n\n{}\n\n{}\n'.format(
+        '<-----------Response-----------',
+        'Status code:' + str(response.status_code),
+        '\n'.join('{}: {}'.format(k, v) for k, v in response.headers.items()),
+        response.text)
+    )
